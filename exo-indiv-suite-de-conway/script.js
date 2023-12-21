fc4c6a4faf72518046d7f037c3dc63cb81b9b242
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2023-12-21 13:14:28 +0100

    affichage DOM suite de conway

--- exo-indiv-suite-de-conway/script.js
+++ exo-indiv-suite-de-conway/script.js
@@ -1,47 +1,52 @@
 
+const affichageBox = document.querySelector('.box')
+
 
 //ETAPE 1 :
 function decoupeChaine(string) {
-    let SeparatedString = string
-    .split('')
-    const newArray = [];
-    for (let i = 0; i<SeparatedString.length; i++){
-
-        if(SeparatedString[i]!==SeparatedString[i+1]){
-            newArray.push(SeparatedString[i]);
-            newArray.push(' ');
-        }
-        else {
-            newArray.push(SeparatedString[i]);
-        }
+  let separatedString = string.split("");
+  const newArray = [];
+  for (let i = 0; i < separatedString.length; i++) {
+    if (separatedString[i] !== separatedString[i + 1]) {
+      newArray.push(separatedString[i]);
+      newArray.push(" ");
+    } else {
+      newArray.push(separatedString[i]);
     }
-    //console.log(newArray.join(''));
-    return decritChaine(newArray.join(''));
+  }
+  //console.log(newArray.join(''));
+  return decritChaine(newArray.join(""));
 }
 //Etape2 :
-function decritChaine(cuttedString){
-    const cuttedSplit = cuttedString.split(' ');
-    //console.log(cuttedSplit);
+function decritChaine(cuttedString) {
+  const cuttedSplit = cuttedString.split(" ");
+  //console.log(cuttedSplit);
 
-    let array = [];
+  let array = [];
 
-    for (let i = 0; i < cuttedSplit.length; i++) {
-        if(cuttedSplit[i].length>0){
-        array.push(cuttedSplit[i].length)
-        array.push(cuttedSplit[i][0])
-        //array.push(' ')
+  for (let i = 0; i < cuttedSplit.length; i++) {
+    if (cuttedSplit[i].length > 0) {
+      array.push(cuttedSplit[i].length);
+      array.push(cuttedSplit[i][0]);
+      //array.push(' ')
     }
-    }
-    //console.log(array.join(''))
-    return array.join('')
+  }
+  //console.log(array.join(''))
+  return array.join("");
+}
+
+function suiteConway(carac, stage) {
+    let finalArray = []
+  for (let i = 0; i < stage; i++) {
+    carac = decoupeChaine(carac);
+    finalArray.push(carac)
+    finalArray.push('<br>')
+
+  }
+  affichageBox.innerHTML = `${finalArray.join('')}`
 }
 
 
-function suiteConway(carac, stage){
-for (let i =0; i<stage; i++){
-    carac = decoupeChaine(carac)
-    console.log(carac)
-   }
-}
 
-suiteConway('x',10)
+
+//ETAPE 4 :
